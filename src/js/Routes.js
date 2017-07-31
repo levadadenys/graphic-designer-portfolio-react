9b4344d966f957e87d52341e1ba0c275f4dff54a
DenysLevada <you@example.com>
2017-08-01 01:00:18 +0300

    moved app.js to js folder, created routes template

---
+++ src/js/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AboutPage from './pages/AboutPage';
+import SkillsetPage from './pages/SkillsetPage';
+import PortfolioPage from './pages/PortfolioPage';
+import OpenSourcePage from './pages/OpenSourcePage';
+import ContactsPage from './pages/ContactsPage';
+
+export default ({});
